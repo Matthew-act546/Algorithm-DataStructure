d1d2252532c3a945e7e18e3f1c1365b104a07b84
matthew <matthewyt765@gmail.com>
2023-11-28 14:55:30 +0800

    Fix codes

--- linked_list_merge_sort.py
+++ linked_list_merge_sort.py
@@ -41,27 +41,56 @@ def split(linked_list):
         return left_half, right_half
 
 def merge(left, right):
-    result = []
-    left_index = 0
-    right_index = 0
+    """
+    Merges two linked list (left and right), sorting by data in nodes
+    Returns a new, merged list.
+    """
 
-    while left_index < len(right) and right_index < len(right):
-        if left[left_index] < right[right_index]:
-            result.append(left[left_index])
-            left_index += 1
+    # Create a new linked list that contains nodes from
+    # Merging left and right.
+    merged= LinkedList()
+
+    # Add a fake head that is discarded it later
+    merged.add(0)
+
+    # Set current to the head of the linked list
+    current = merged.head
+
+    # Obtain head nodes from the left and right of the linked list  
+    left_head = left.head
+    right_head = right.head
+
+    # Iterate over left and right until we reach the tail node of either
+    while left_head or right_head:
+        # If the head node of left is none, were past the tail node
+        # Add the node from right to merged linked list
+        if left_head is None:
+            current.next_node = right_head 
+            # Call next on right to set the loop fucnctions to False 
+            right_head = right_head.next_node
+        # If the head node of right is None, we're past the tail 
+        # Add the tail node from the left to merged linked list.
+        elif right_head is None:
+            current.next_node = left_head
+            # Call next on left to set the loop fucnctions to False 
+            left_head = left_head.next_node
         else:
-            result.append(right[right_index])
-            right_index += 1
+            # Nota at either tail node
+            # Obtain node data to perform comparison operations 
+            left_data = left_head.data
+            right_data = right_head.data
+            # If data on left is less than right, set current to left node
+            if left_data < right_data:
+                current.next_node = left_head
+                # Move left head to next node
+                left_head = left_head.next_node
+            # If data on left is greater than right, set current to left node
+            else:
+                current.next_node = right_head
+                # move right to next node
+                right_head = right_head.next_node
 
-    while left_index < len(left):
-        result.append(left[left_index])
-        left_index += 1
 
-    while right_index < len(right):
-        result.append(right[right_index])
-        right_index += 1
-
-    return result
 
 def verify_sorted(linked_list):
     n = linked_list.size()
@@ -73,8 +102,3 @@ def verify_sorted(linked_list):
         flag =  True
 
     return ((linked_list[0] < linked_list[1] or flag) and verify_sorted(linked_list[1:]))
-
-
-
-
-
